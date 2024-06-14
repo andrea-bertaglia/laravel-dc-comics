df3d364c288fa93ca884b8769922c8b36f57daeb
andrea-bertaglia <andreabertaglia@gmail.com>
2024-06-14 19:07:29 +0200

    feat: define index function in controller

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,5 +1,38 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>prova</h1>
+    <div class="container">
+        <h1 class="text-center py-4 fw-bold">Lista DC Comics</h1>
+        <div class="row g-4">
+            @foreach ($comics as $comic)
+                <div class="col-4">
+                    <div class="card h-100 w-100" style="width: 18rem;">
+                        <img src="{{ $comic->thumb }}" class="card-img-top" alt="Copertina di {{ $comic->title }}">
+                        <div class="card-body">
+                            <h5 class="card-title fw-bold py-2">{{ $comic->title }}</h5>
+                            <p class="card-text">{{ $comic->description }}</p>
+                        </div>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">
+                                <span class="fw-bold">Serie: </span>
+                                <span>{{ $comic->series }}</span>
+                            </li>
+                            <li class="list-group-item">
+                                <span class="fw-bold">Data di uscita: </span>
+                                <span>{{ $comic->sale_date }}</span>
+                            </li>
+                            <li class="list-group-item">
+                                <span class="fw-bold">Tipo: </span>
+                                <span>{{ $comic->type }}</span>
+                            </li>
+                        </ul>
+                        <div class="card-body">
+                            <span class="fw-bold">Prezzo: </span>
+                            <span>{{ $comic->price }} $</span>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
 @endsection
